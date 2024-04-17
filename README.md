bef976bb50e75ac4c34f6446cb93f04abf046d5f
Miina Saroma <miina.saroma@helsinki.fi>
2024-04-17 23:15:35 +0300

    Add WIP login functionality

--- README.md
+++ README.md
@@ -3,43 +3,41 @@
 Tietokannat ja web-ohjelmointiprojekti 2024
 
 
-Tämänhetkinen toiminnallisuus:
+### Tämänhetkinen toiminnallisuus:
+* Voi rekisteröityä.
+* Voi kirjautua.
+* Voi kirjautuneena katsoa listaa muista käyttäjistä.
 
-Voi lisätä uuden käyttäjän.
-Voi valita kolmesta eri alasta yhden.
-Ei validointia syötteelle.
-Ei vielä swipeämis-funktionaalisuutta.
-
-
-
-Taulujen luontia varten:
-
-		CREATE TABLE studyfields (id SERIAL PRIMARY KEY, field TEXT);
-		INSERT INTO studyfields (field) VALUES ('Computer Science');
-		INSERT INTO studyfields (field) VALUES ('Math');
-		INSERT INTO studyfields (field) VALUES ('Physics');
-
-		CREATE TABLE users (id SERIAL PRIMARY KEY, name TEXT, studyfield_id INTEGER REFERENCES studyfields, bio TEXT);
-		INSERT INTO users (name, studyfield_id, bio) VALUES ('Maija', '1', 'Moi oon Maija ja olen hauki');
-		INSERT INTO users (name, studyfield_id, bio) VALUES ('Joonas', '3', 'gucci laif and all things topology');
-
-		CREATE TABLE swipes (id SERIAL PRIMARY KEY, swiper_id INTEGER REFERENCES users, swipee_id INTEGER REFERENCES users);
-		
-		INSERT INTO swipes VALUES (1, 2);
+#### Mitä esim puuttuu:
+* Ei validointia syötteelle.
+* Ei vielä tykkäystoiminnallisuutta.
+* Ei vielä profiilin muokkaamista jne.
 
 
+### Käyttöohje:
 
-Alla ajatuksia, keskeneräisiä:
+Luo juureen tiedosto `.env` ja aseta sinne `DATABASE_URL` sekä `SECRET_KEY`
 
+Lataa riippuvuudet `pip install -r requirements.txt`
+
+Luo tietokantataulut `psql > schema.sql`
+
+Jos haluat poistaa sovelluksen taulut, käytä `psql > drop.sql`
+
+`psql > userdata.sql` sisältää useita valmiita profiileja, joiden kaikkien salasana on `salasana`.
+
+
+
+#### TODO:
 - yksinkertainen UI
+- tekstikenttien validointi jne
+- UUID:t
 
 Käyttäjä:
-- voi tehdä tilin ja kirjautua 
 - voi selata muita käyttäjiä:
-	- kaikkia käyttäjiä vai rajoitettu omien ominaisuuksien tai haettavan seuran mukaan?
+	- kaikkia käyttäjiä? vai rajoitettu omien ominaisuuksien tai haettavan seuran mukaan?
 - voi osoittaa kiinnostuksensa (klikata checkboxin tmv)
 - tulee ilmoitus jos molemmat ovat valinneet toisensa
-    - mahdollisesti näissä matchausjutuissa useita eri aihepiirejä (halailu, kaveri, seksi, jne)
 
 Ehkä:
 - tilin poistaminen
@@ -51,51 +49,12 @@ Ehkä:
 
 
 Lista pohdituista tauluista:
-
-- käyttäjät:
-	* uuid
-	* rooli: admin/user? (mitä admin voi tehdä?)
-	* nimi,
-	* opiskeluala: reference,
-	* aloitusvuosi: optional?
-  * kuva (referenssi kuvatauluun)
-  * profiiliteksti (vai nämä erilliseen tietokantaan ja referenssi?)
-  * telegram-tunnus: ehkä jaettu vasta jos match
-
-	* suhdetyyli (mono/poly/anarkisti/ambi/en tiedä): optional?
-	* alkoholitaulukko: optional?
-
-	* lempikurssi?
-  * lempitapahtuma?
-
-- opiskelualat: [kiinteä listaus]
-
-- matchaus-taulu: liitostaulu (ketkä valinneet toisen)
-	* ehkä useampia asioita joissa match, jokaisesta osiosta olisi oma taulunsa
-		* cuddling, romantic, sexual, friendship? entä opiskeluseura?
+* ehkä useampia like-tauluja jokaisesta osiosta olisi oma taulunsa
+	* cuddling, romantic, sexual, friendship? entä opiskeluseura?
 
 - kuvat: tietokanta niille, blobeina säilöttynä tai path. ehkä vain yksi kuva per henkilö, onko optional?
 
 
 Ajatuksia:
 - olisiko mukavampi tietokannan käyttämisen suhteen, jos profiilin ei-kirjautumistiedot olisi eritelty "profiili"-tauluun, jossa mm. nimi, ala, aloitusvuosi, kuva, teksti ja muut tiedot
-
-- muista lisätä UUID:t myöhemmin
-- references vai foreign key?
-
-
-ehkä validoi:
-  - onks telegram_nikki tarpeeks pitkä/lyhyt
-  - kans bioon
-
-- mitä tapahtuu jos joku poistaa profiilin ja se on swipennyt tai sitä on swipetty?
-
-Todo esim:
-- kirjautuminen
-- erilliset käyttäjät
-- bion muokkaaminen
-- useampi "swipe"-taulu
-- taulujen initioiminen 
-- voi klikata match
-	- se tallentuu
-	- jos molemmissa: joku ilmoituss
+- mitä tapahtuu jos joku poistaa profiilin ja on "matchatty"?
--- app.py
+++ app.py
@@ -1,23 +1,57 @@
 from sqlalchemy.sql import text
 from flask import Flask
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 from os import getenv
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key = getenv("SECRET_KEY")
+
 db = SQLAlchemy(app)
+
+
+### TODO:
+#       - like function/checkbox tjsp. form :D
+#       - sit kans cancel option
+#       - kirjautumistoiminto           !!!
+#       - profiilin muokkaaminen
+#       - handle tyhjät/jne/validoi/jne form jne
+#       - hakuominaisuutta?
+
 
 @app.route("/")
 def index():
-    result = db.session.execute(text("SELECT name, studyfield_id, bio FROM users"))
+    return render_template("index.html")
+
+
+# TODO: älä näytä omaa profiilia listauksessa
+@app.route("/profiles")
+def profiles():
+    # result = db.session.execute(text("SELECT name, studyfield_id, bio FROM users"))
+    command = "SELECT name, studyfields.field, bio FROM users LEFT JOIN studyfields ON studyfields.id = users.studyfield_id"
+    # command = "SELECT username, name, studyfields.field, bio FROM users LEFT JOIN studyfields ON studyfields.id = users.studyfield_id WHERE username !=:username"
+
+    # result = db.session.execute(text(command), {"username": session.username})
+    result = db.session.execute(text(command))
     users = result.fetchall()
-    return render_template("dbindex.html", count=len(users), users=users)
+    return render_template("profiles.html", count=len(users), users=users)
+
 
 @app.route("/new")
 def new():
     return render_template("new.html")
+
+
+def get_field_id(fieldname):
+    result = db.session.execute(
+        text("SELECT id FROM studyfields WHERE field=:field;"), {"field": fieldname}
+    )
+    return result.fetchone()[0]
+
 
 @app.route("/send", methods=["POST"])
 def send():
@@ -26,13 +60,95 @@ def send():
     bio = request.form["bio"]
 
     # haetaan id ensin
-    result = db.session.execute(text("SELECT id FROM studyfields WHERE field=:field;"), {"field": field})
-    id = result.fetchone()
-    id = id[0]
+    id = get_field_id(field)
 
-    sql = text("INSERT INTO users (name, studyfield_id, bio) VALUES (:name, :studyfield_id, :bio)")
+    sql = text(
+        "INSERT INTO users (name, studyfield_id, bio) VALUES (:name, :studyfield_id, :bio)"
+    )
 
     db.session.execute(sql, {"name": name, "studyfield_id": id, "bio": bio})
 
     db.session.commit()
+    return redirect("/profiles")
+
+
+@app.route("/sendregister", methods=["POST"])
+def sendregister():
+    # TODO:
+    # - käyttäjänimen validointi (pituus jne)
+    # - salasanan valdointi
+    # - errors
+    # - kamalaa joutua täyttää uudestaan jos ei saa
+    #           luotua, mutta tehdään nyt näin
+
+    username = request.form["username"]
+    passw = request.form["passw"]
+    name = request.form["name"]
+    field = request.form["field"]
+    bio = request.form["bio"]
+
+    # haetaan usernamella ensin
+    result = db.session.execute(
+        text("SELECT username FROM users WHERE username=:username;"),
+        {"username": username},
+    )
+    user = result.fetchone()
+
+    if user:
+        # TODO: Error1!
+        print("error: user exists")
+    else:
+        passw_hashed = generate_password_hash(passw)
+        id = get_field_id(field)
+
+        sql = text(
+            "INSERT INTO users (username, passw, name, studyfield_id, bio) VALUES (:username, :passw, :name, :studyfield_id, :bio)"
+        )
+
+        db.session.execute(
+            sql,
+            {
+                "username": username,
+                "passw": passw_hashed,
+                "name": name,
+                "studyfield_id": id,
+                "bio": bio,
+            },
+        )
+        db.session.commit()
+    return redirect("/new")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    sql = text("SELECT id, passw FROM users WHERE username=:username")
+    result = db.session.execute(sql, {"username": username})
+    user = result.fetchone()
+    if not user:
+        # TODO: invalid username
+        print("Error: invalid username")
+    else:
+        hash_value = user.passw
+        if check_password_hash(hash_value, password):
+            # TODO: correct username and password
+            print("Jee läpi meni")
+        else:
+            # TODO: invalid password
+            print("Error: väärä salasana")
+
+    session["username"] = username
+    return redirect("/profiles")
+
+
+@app.route("/logout")
+def logout():
+    del session["username"]
     return redirect("/")
